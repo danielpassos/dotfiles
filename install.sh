658af8d84d103435bed0a77e97feb719d4f94408
Daniel Passos <danpassos@gmail.com>
2010-06-26 11:49:14 -0300

    Delete all terminal bundle

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 # Bash Files
 
-mkdir ~/bin/dotfiles/bash
+mkdir -p ~/bin/dotfiles/bash
 cp ./bashrc  ~/bin/dotfiles/
 cp ./bash/aliases ~/bin/dotfiles/bash/
 cp ./bash/bash_prompt_color ~/bin/dotfiles/bash/
@@ -9,14 +9,9 @@ cp ./bash/env ~/bin/dotfiles/bash/
 
 # Scripts Files
 
-mkdir ~/bin/dotfiles/script
+mkdir -p ~/bin/dotfiles/script
 cp ./script/gem_completion.sh ~/bin/dotfiles/script/
 cp ./script/git_completion.sh ~/bin/dotfiles/script/
-
-#Terminal
-
-cp -a ./bundle/terminal/TerminalColours.bundle /Library/Application\ Support/SIMBL/Plugins/
-cp -a ./bundle/terminal/TerminalTabSwitching.bundle /Library/Application\ Support/SIMBL/Plugins/
 
 # Links
 
