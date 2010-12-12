d561bb0d91557d75c4b6d7cdfb24567b13a5aa88
Daniel Passos <danpassos@gmail.com>
2010-12-12 17:30:32 -0200

    Add .bash_profile

--- install.sh
+++ install.sh
@@ -1,18 +1,15 @@
 # Bash Files
 
+cp ./.bash_profile ~/
+
 mkdir -p ~/bin/dotfiles/bash
-cp ./bash/bash_prompt_color ~/bin/dotfiles/bash/
 cp ./bashrc  ~/bin/dotfiles/
-cp ./bash/aliases ~/bin/dotfiles/bash/
-cp ./bash/config ~/bin/dotfiles/bash/
-cp ./bash/env ~/bin/dotfiles/bash/
-cp ./bash/path ~/bin/dotfiles/bash/
+cp ./bash/* ~/bin/dotfiles/bash/
 
 # Scripts Files
 
 mkdir -p ~/bin/dotfiles/script
-cp ./script/gem_completion.sh ~/bin/dotfiles/script/
-cp ./script/git_completion.sh ~/bin/dotfiles/script/
+cp ./script/* ~/bin/dotfiles/script/
 
 # Links
 
