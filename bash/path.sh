47a9e9085871ab1e4db4a16059d6e741ca95c66d
Daniel Passos <daniel@passos.me>
2016-10-25 09:37:54 -0200

    Add homebrew/bash-completion

--- bash/path.sh
+++ bash/path.sh
@@ -6,6 +6,12 @@ PATH=.:$PATH
 PATH=$PATH:$HOME/.dotfiles/bin
 PATH=$PATH:/usr/local/bin:/usr/local/sbin
 
+# RVM
 if [ -d $HOME/.rvm ] ; then
     PATH=$PATH:$HOME/.rvm/bin
+fi
+
+# homebrew/bash-completion
+if [ -f $(brew --prefix)/etc/bash_completion ]; then
+	. $(brew --prefix)/etc/bash_completion
 fi
