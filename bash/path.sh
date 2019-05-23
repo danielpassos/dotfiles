ecc62be809c095904de36ff300a530b65ad6d280
Daniel Passos <daniel@passos.me>
2019-05-23 14:45:52 -0300

    Cleanup path

--- bash/path.sh
+++ bash/path.sh
@@ -4,13 +4,3 @@
 
 PATH=.:$PATH
 PATH=$PATH:$HOME/.dotfiles/bin
-
-# RVM
-if [ -d $HOME/.rvm ] ; then
-    PATH=$PATH:$HOME/.rvm/bin
-fi
-
-# Rust
-if [ -d $HOME/.rvm ] ; then
-    PATH=$PATH:$HOME/.cargo/bin:$PATH
-fi
