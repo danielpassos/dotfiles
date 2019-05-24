106f7ff43587572bf61c2914628b6c89b8408f92
Daniel Passos <daniel@passos.me>
2019-05-24 17:28:46 -0300

    Improve zsh installation process

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -3,39 +3,40 @@
 zsh_mac ()
 {
 	# Install Brew
-	type brew >/dev/null 2>&1 || {
-		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-	}
+	if [ ! -x "$(command -v brew)" ]; then
+		/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" >> /dev/null
+	fi	
 
 	# Install iTerm2
 	if [ ! -d /Applications/iTerm.app ]; then
-	    brew cask install iterm2
+	    brew cask install iterm2 >> /dev/null
 	fi
 
 	# Install zsh
-	type zsh >/dev/null 2>&1 || {
-		brew install zsh
-	}
+	if [ ! -x "$(command -v zsh)" ]; then
+		brew install zsh >> /dev/null
+	fi
 
 	# Install oh-my-zsh
 	if [ ! -d ~/.oh-my-zsh/ ]; then
-	    curl -O https://raw.githubusercontent.com/MartinSeeler/iterm2-material-design/master/material-design-colors.itermcolors
+	    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" >> /dev/null
 	fi
 
 	# Install powerlevel9k
 	if [ ! -d /usr/local/opt/powerlevel9k/ ]; then
-	    brew tap sambadevi/powerlevel9k && brew install powerlevel9k
+	    brew tap sambadevi/powerlevel9k >> /dev/null
+	    brew install powerlevel9k >> /dev/null
 	fi
 
 	# Install zsh-autosuggestions
 	if [ ! -f /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh ]; then
-	    brew install zsh-autosuggestions
+	    brew install zsh-autosuggestions >> /dev/null
 	fi
 
-	# Install colorsls
-	type zsh >/dev/null 2>&1 || {
-		gem install colorls
-	}
+	# Install colorls
+	if [ ! -x "$(command -v colorls)" ]; then
+		gem install colorls >> /dev/null
+	fi
 
 	cp ./zsh/zshrc ~/.zshrc
 }
