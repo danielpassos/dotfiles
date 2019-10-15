95289b1b7cf471a1c3c23558ce43930a771f23ba
Daniel Passos <daniel@passos.me>
2019-10-15 14:11:28 -0300

    Fix nvm check

--- node/nvm.zsh
+++ node/nvm.zsh
@@ -1,4 +1,4 @@
-if test "$(command -v nvm)"; then
+if [ -d $HOME/.nvm ]; then
 	export NVM_DIR="$HOME/.nvm"
 	[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 	[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
