0e09df9384452dfa28c726f7aad96f720b9f8c82
Daniel Passos <daniel@passos.me>
2019-10-15 10:07:45 -0300

    Fix iTerm check

--- iterm/env.zsh
+++ iterm/env.zsh
@@ -1,5 +1,9 @@
-if test "$(uname)" = "Darwin"; then
+if [ $TERM_PROGRAM = "iTerm.app" ]; then
+  DISABLE_AUTO_TITLE="true"
+
   echo -e "\033]6;1;bg;red;brightness;18\a"
   echo -e "\033]6;1;bg;green;brightness;26\a"
   echo -e "\033]6;1;bg;blue;brightness;33\a"
 fi
+
+
