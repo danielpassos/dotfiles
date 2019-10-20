a868461d5516b45218e4cb168d0ba9f30b910d34
Daniel Passos <daniel@passos.me>
2019-10-20 13:08:01 -0300

    Killing autocompletion.zsh option

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,13 +1,19 @@
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
 
-fpath=($ZSH/functions $fpath)
+autoload -U compinit
+compinit
 
+# shellcheck disable=SC2206
+fpath=($ZSH/functions $fpath)
 autoload -U "$ZSH/functions/*(:t)"
 
-HISTFILE=~/.zsh_history
-HISTSIZE=10000
-SAVEHIST=10000
+export SAVEHIST=10000
+export HISTSIZE=10000
+export HISTFILE=~/.zsh_history
+
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+zstyle ':completion:*' insert-tab pending
 
 setopt NO_BG_NICE # don't nice background tasks
 setopt NO_HUP
@@ -20,10 +26,8 @@ setopt EXTENDED_HISTORY # add timestamps to history
 setopt PROMPT_SUBST
 setopt COMPLETE_IN_WORD
 setopt IGNORE_EOF
-
 setopt APPEND_HISTORY # adds history
 setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
 setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
-
-setopt complete_aliases
+setopt COMPLETE_ALIASES
