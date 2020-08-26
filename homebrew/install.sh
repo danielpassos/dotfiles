97c9941649c998accbbfc3987009684751cca189
Daniel Passos <daniel@passos.me>
2020-08-26 15:35:23 -0400

    Make home brew required

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,13 +1,3 @@
-# Install Homebrew
-if test ! "$(command -v brew)"; then
-  echo "Installing Homebrew for you."
-  if test "$(uname)" = "Darwin"; then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  elif test "$(uname)" = "Linux"; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
-  fi
-fi
-
 # Install brew apps
 brew bundle --file=./homebrew/Brewfile
 
