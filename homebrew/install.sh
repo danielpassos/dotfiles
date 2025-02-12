8001ecb88a2c1996bbf6c1896e7ac33e56e5eaef
Daniel Passos <daniel@passos.me>
2025-02-11 21:30:20 -0300

    Update defauld brew apps installation

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,7 +1,7 @@
-echo "* Installing brew apps"
+echo "* Installing brew dotfiles apps"
 brew bundle --file=./homebrew/Brewfile >/dev/null
 
 if [[ -e ~/.Brewfile ]]; then
-  echo "* Installing brew apps (~/.Brewfile)"
+  echo "* Installing brew personal apps (~/.Brewfile)"
   brew bundle --file=~/.Brewfile >/dev/null
 fi
