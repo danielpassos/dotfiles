4901fe70d001ed808663f93a64b0c85ed154e229
Daniel Passos <daniel@passos.me>
2025-02-11 21:42:07 -0300

    Cleanup zsh configurations

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,33 +1,10 @@
-export LSCOLORS="exfxcxdxbxegedabagacad"
-export CLICOLOR=true
+##############################################################################
+# History Configuration                                                      #
+##############################################################################
+export HISTFILE=$HOME/.zsh_history
+export SAVEHIST=10000    # Number of history entries to save to disk
+export HISTSIZE=10000    # Lines of history to keep in memory
 
-autoload -U compinit
-compinit
-
-# shellcheck disable=SC2206
-fpath=($ZSH/functions $fpath)
-autoload -U "$ZSH/functions/*(:t)"
-
-export SAVEHIST=10000
-export HISTSIZE=10000
-export HISTFILE=~/.zsh_history
-
-zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
-zstyle ':completion:*' insert-tab pending
-
-setopt NO_BG_NICE # don't nice background tasks
-setopt NO_HUP
-setopt NO_LIST_BEEP
-setopt LOCAL_OPTIONS # allow functions to have local options
-setopt LOCAL_TRAPS # allow functions to have local traps
-setopt HIST_VERIFY
-setopt SHARE_HISTORY # share history between sessions ???
-setopt EXTENDED_HISTORY # add timestamps to history
-setopt PROMPT_SUBST
-setopt COMPLETE_IN_WORD
-setopt IGNORE_EOF
-setopt APPEND_HISTORY # adds history
-setopt INC_APPEND_HISTORY SHARE_HISTORY  # adds history incrementally and share it across sessions
-setopt HIST_IGNORE_ALL_DUPS  # don't record dupes in history
-setopt HIST_REDUCE_BLANKS
-setopt COMPLETE_ALIASES
+setopt appendhistory     # Append history to the file (no overwriting)
+setopt sharehistory      # Share history across terminals
+setopt incappendhistory  # Immediately append to the file, not when term killed
