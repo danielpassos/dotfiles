6ea9febd3228c13c45f9678c6000fa7d9a32d919
Daniel Passos <daniel@passos.me>
2025-02-13 09:37:43 -0300

    Update wezterm config with frame changes

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,4 +5,12 @@ config.color_scheme = 'Hacktober'
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 18
 
+config.window_frame = {
+  font = require('wezterm').font 'Roboto',
+  font_size = 18,
+}
+config.window_close_confirmation = 'NeverPrompt'
+
+use_fancy_tab_bar = false
+
 return config
