47ed3d23bd9cbbdcdd1441efe100652ea1af260d
Daniel Passos <daniel@passos.me>
2025-02-17 13:43:52 -0300

    Update the SDKMAN installation check based on homebrew install

--- sdkman/sdkman.zsh
+++ sdkman/sdkman.zsh
@@ -1,5 +1,3 @@
-if test "$(command -v sdk)"; then
-  # shellcheck disable=SC2155
-  export SDKMAN_DIR=$(brew --prefix sdkman-cli)/libexec
-  [[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
-fi
+# shellcheck disable=SC2155
+export SDKMAN_DIR=$(brew --prefix sdkman-cli)/libexec
+[[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
