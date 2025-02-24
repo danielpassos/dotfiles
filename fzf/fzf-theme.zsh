562a121a35eda587f8b22c72ff7ae4aa8d3094b3
Daniel Passos <daniel@passos.me>
2025-02-23 23:30:14 -0300

    Update fzf config

---
+++ fzf/fzf-theme.zsh
@@ -0,0 +1,9 @@
+# --- setup fzf theme ---
+fg="#CBE0F0"
+bg="#011628"
+bg_highlight="#143652"
+purple="#B388FF"
+blue="#06BCE4"
+cyan="#2CF9ED"
+
+export FZF_DEFAULT_OPTS="--color=fg:${fg},bg:${bg},hl:${purple},fg+:${fg},bg+:${bg_highlight},hl+:${purple},info:${blue},prompt:${cyan},pointer:${cyan},marker:${cyan},spinner:${cyan},header:${cyan}"
