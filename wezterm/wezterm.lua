ae036b6856331d23374cd9e67518d6165d6d7d75
Daniel Passos <daniel@passos.me>
2025-02-24 15:33:26 -0300

    Add WezTerm shotcut to rename the tab

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
+local act = wezterm.action
 
 config.color_scheme = 'Hacktober'
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
@@ -10,6 +11,25 @@ config.window_frame = {
   font_size = 18,
 }
 config.window_close_confirmation = 'NeverPrompt'
+
+config.keys = {
+  # https://wezterm.org/config/lua/keyassignment/PromptInputLine.html
+  {
+    key = 'E',
+    mods = 'CTRL|SHIFT',
+    action = act.PromptInputLine {
+      description = 'Enter new name for tab',
+      action = wezterm.action_callback(function(window, pane, line)
+        -- line will be `nil` if they hit escape without entering anything
+        -- An empty string if they just hit enter
+        -- Or the actual line of text they wrote
+        if line then
+          window:active_tab():set_title(line)
+        end
+      end),
+    },
+  }
+}
 
 use_fancy_tab_bar = false
 
