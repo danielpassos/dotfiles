73a8a51d543810df078580a43c7729bd5dd98524
Daniel Passos <daniel@passos.me>
2026-03-22 21:29:40 -0300

    Remove personal alias for Brewfile access

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -10,8 +10,6 @@ alias psgrep="ps aux | egrep -v egrep | egrep"
 
 alias pubkey="more ~/.ssh/id_rsa.pub | pbcopy | echo '=> Public key copied to pasteboard.'"
 
-alias obf="open ~/.Brewfile"
-
 alias uuid='uuidgen | tr "[:upper:]" "[:lower:]"'
 
 alias reload!='. ~/.zshrc && clear'
